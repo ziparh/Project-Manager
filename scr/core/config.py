25561d4b643cd3be5635513c533f7cacb383a940
ziparh <thelackutop@gmail.com>
2025-10-13 23:20:05 +0200

    Add database connection and session factory

--- scr/core/config.py
+++ scr/core/config.py
@@ -8,12 +8,18 @@ class RunConfig(BaseModel):
     reload: bool = True
 
 
+class DatabaseConfig(BaseModel):
+    url: str = 'sqlite+aiosqlite:///./database.db'
+    echo: bool = True
+    echo_pool: bool = True
+
 class PrefixConfig(BaseModel):
     api_v1: str = '/api/v1'
 
 
 class Settings(BaseSettings):
     run: RunConfig = RunConfig()
+    db: DatabaseConfig = DatabaseConfig()
     prefix: PrefixConfig = PrefixConfig()
 
 
---
+++ scr/db/session.py
@@ -0,0 +1,26 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+
+from core.config import settings
+
+engine = create_async_engine(
+    url=settings.db.url,
+    echo=settings.db.echo,
+    echo_pool=settings.db.echo_pool,
+)
+
+async_session_fabric = async_sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    autocommit=False,
+    autoflush=False,
+    expire_on_commit=False,
+)
+
+
+async def get_session():
+    async with async_session_fabric() as sess:
+        try:
+            yield sess
+        except Exception:
+            await sess.rollback()
+            raise
