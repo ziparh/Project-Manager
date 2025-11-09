b997215cdc4904b5f7b63650accfb42394dbe4c3
ziparh <thelackutop@gmail.com>
2025-11-09 20:38:41 +0100

    Add .env support to settings

--- src/core/config.py
+++ src/core/config.py
@@ -1,5 +1,8 @@
+import os
 from pydantic import BaseModel
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+env_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.env"))
 
 
 class RunConfig(BaseModel):
@@ -8,30 +11,36 @@ class RunConfig(BaseModel):
     reload: bool = True
 
 
-class DatabaseConfig(BaseModel):
-    url: str = "sqlite+aiosqlite:///./database.db"
-    echo: bool = True
-    echo_pool: bool = True
-
-
 class PrefixConfig(BaseModel):
     api_v1: str = "/api/v1"
     users: str = "/users"
     auth: str = "/auth"
 
 
+class DatabaseConfig(BaseModel):
+    url: str
+    echo: bool = True
+    echo_pool: bool = True
+
+
 class AuthJWTConfig(BaseModel):
+    secret_key: str
     algorithm: str = "HS256"
-    secret_key: str = "secret"
     access_token_expire: int = 60 * 3  # seconds * minutes
     refresh_token_expire: int = 60 * 60 * 24 * 14  # seconds * minutes * hours * days
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=env_path,
+        case_sensitive=False,
+        env_nested_delimiter="__",
+        env_prefix="APP_CONFIG__",
+    )
     run: RunConfig = RunConfig()
-    db: DatabaseConfig = DatabaseConfig()
     prefix: PrefixConfig = PrefixConfig()
-    jwt: AuthJWTConfig = AuthJWTConfig()
+    db: DatabaseConfig
+    jwt: AuthJWTConfig
 
 
 settings = Settings()
