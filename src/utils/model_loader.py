810ffb752d27e1bf79819c815a87b95dd9826fa2
ziparh <thelackutop@gmail.com>
2025-11-09 19:21:13 +0100

    fix ModuleNotFoundError in model_loader

--- src/utils/model_loader.py
+++ src/utils/model_loader.py
@@ -4,4 +4,7 @@ import pkgutil
 import modules
 
 for _, module_name, _ in pkgutil.iter_modules(modules.__path__):
-    importlib.import_module(f"modules.{module_name}.model")
+    try:
+        importlib.import_module(f"modules.{module_name}.model")
+    except ModuleNotFoundError:
+        pass
