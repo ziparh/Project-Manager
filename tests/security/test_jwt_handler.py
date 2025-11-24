07cee87163c3e5522e12af0e5d281b663bdf78a0
ziparh <thelackutop@gmail.com>
2025-11-24 23:43:52 +0100

    Add time_machine to jwt handler test

--- tests/security/test_jwt_handler.py
+++ tests/security/test_jwt_handler.py
@@ -1,11 +1,11 @@
 import pytest
+import time_machine
 from unittest.mock import MagicMock
 from datetime import datetime, timezone
 
 from core.security.jwt_handler import JWTHandler
 from core.config import settings
 from enums.token import TokenType
-
 
 
 @pytest.mark.parametrize(
@@ -21,38 +21,25 @@ def test_tokens_creation_and_decoding_success(
     user_id: int,
     expected_lifetime: int,
 ):
-    time_before_creation = datetime.now(timezone.utc).timestamp() - 0.1
+    fixed_dt = datetime(2025, 1, 1, 12, 0, 0, tzinfo=timezone.utc)
 
-    token = JWTHandler.create(
-        user_id=user_id,
-        token_type=token_type,
-    )
+    with time_machine.travel(fixed_dt, tick=False):
+        token = JWTHandler.create(
+            user_id=user_id,
+            token_type=token_type,
+        )
 
-    time_after_creation = datetime.now(timezone.utc).timestamp() + 0.1
+        assert isinstance(token, str)
+        assert len(token) > 10
 
-    assert isinstance(token, str)
-    assert len(token) > 10
+        payload = JWTHandler.decode(token)
 
-    payload = JWTHandler.decode(token)
+        assert isinstance(payload, dict)
+        assert payload["type"] == token_type.value
+        assert payload["sub"] == str(user_id)
 
-    assert isinstance(payload, dict)
-    assert payload["type"] == token_type.value
-    assert payload["sub"] == str(user_id)
-
-
-    iat = payload["iat"]
-    assert iat > time_before_creation
-    assert iat < time_after_creation
-
-
-    exp = payload["exp"]
-
-    expected_ext_low = iat + expected_lifetime - 0.1
-    expected_ext_high = iat + expected_lifetime + 0.1
-
-    assert exp > expected_ext_low
-    assert exp < expected_ext_high
-
+        assert payload["iat"] == fixed_dt.timestamp()
+        assert payload["exp"] == fixed_dt.timestamp() + expected_lifetime
 
 def test_decoding_invalid_token():
     invalid_token = "invalid.jwt.token"
