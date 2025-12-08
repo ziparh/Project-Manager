bfd399f8893cde1ee0e9af8646f426d898bcd49f
ziparh <thelackutop@gmail.com>
2025-12-08 18:14:57 +0100

    Fix sorting by priority

--- src/modules/personal_tasks/repository.py
+++ src/modules/personal_tasks/repository.py
@@ -1,59 +1,16 @@
-from sqlalchemy import select, update, delete, or_, and_, func, asc, desc, Select
+from sqlalchemy import select, update, delete, or_, and_, func, asc, desc, Select, case
 from sqlalchemy.ext.asyncio import AsyncSession
+from typing import Sequence
 from datetime import datetime, timezone
 
 from . import model, dto as tasks_dto
 from common import dto as common_dto
-from enums.task import TaskStatus
+from enums.task import TaskStatus, TaskPriority
 
 
 class PersonalTaskRepository:
     def __init__(self, db: AsyncSession):
         self.db = db
-
-    def _apply_filters(
-        self, query: Select, filters: tasks_dto.PersonalTaskFilterDto
-    ) -> Select:
-        if filters.status:
-            query = query.where(model.PersonalTask.status == filters.status)
-
-        if filters.priority:
-            query = query.where(model.PersonalTask.priority == filters.priority)
-
-        if filters.overdue is not None:
-            now = datetime.now(timezone.utc)
-            if filters.overdue:
-                # Overdue = deadline passed AND status not completed/canceled
-                query = query.where(
-                    and_(
-                        model.PersonalTask.deadline < now,
-                        model.PersonalTask.status.notin_(
-                            [TaskStatus.DONE, TaskStatus.CANCELLED]
-                        ),
-                    )
-                )
-            else:
-                # Not Overdue = deadline in the future OR no deadline OR status completed/canceled
-                query = query.where(
-                    or_(
-                        model.PersonalTask.deadline >= now,
-                        model.PersonalTask.deadline.is_(None),
-                        model.PersonalTask.status.in_(
-                            [TaskStatus.DONE, TaskStatus.CANCELLED]
-                        ),
-                    )
-                )
-
-        if filters.search:
-            search_term = f"%{filters.search}%"
-            query = query.where(
-                or_(
-                    model.PersonalTask.title.ilike(search_term),
-                    model.PersonalTask.description.ilike(search_term),
-                )
-            )
-
-        return query
 
     async def get_list(
         self,
@@ -61,7 +18,7 @@ class PersonalTaskRepository:
         filters: tasks_dto.PersonalTaskFilterDto,
         sorting: common_dto.SortingDto,
         pagination: common_dto.PaginationDto,
-    ):
+    ) -> tuple[Sequence[model.PersonalTask], int]:
         # Basic query
         stmt = select(model.PersonalTask).where(model.PersonalTask.user_id == user_id)
 
@@ -73,11 +30,7 @@ class PersonalTaskRepository:
         total = await self.db.scalar(count_query) or 0
 
         # Apply sorting
-        sort_field = getattr(model.PersonalTask, sorting.sort_by)
-        if sorting.order == "asc":
-            stmt = stmt.order_by(asc(sort_field))
-        else:
-            stmt = stmt.order_by(desc(sort_field))
+        stmt = self._apply_sorting(stmt, sorting)
 
         # Apply pagination
         stmt = stmt.limit(pagination.size).offset(pagination.offset)
@@ -127,3 +80,75 @@ class PersonalTaskRepository:
 
         await self.db.execute(stmt)
         await self.db.commit()
+
+    def _apply_filters(
+        self, query: Select, filters: tasks_dto.PersonalTaskFilterDto
+    ) -> Select:
+        if filters.status:
+            query = query.where(model.PersonalTask.status == filters.status)
+
+        if filters.priority:
+            query = query.where(model.PersonalTask.priority == filters.priority)
+
+        if filters.overdue is not None:
+            now = datetime.now(timezone.utc)
+            if filters.overdue:
+                # Overdue = deadline passed AND status not completed/canceled
+                query = query.where(
+                    and_(
+                        model.PersonalTask.deadline < now,
+                        model.PersonalTask.status.notin_(
+                            [TaskStatus.DONE, TaskStatus.CANCELLED]
+                        ),
+                    )
+                )
+            else:
+                # Not Overdue = deadline in the future OR no deadline OR status completed/canceled
+                query = query.where(
+                    or_(
+                        model.PersonalTask.deadline >= now,
+                        model.PersonalTask.deadline.is_(None),
+                        model.PersonalTask.status.in_(
+                            [TaskStatus.DONE, TaskStatus.CANCELLED]
+                        ),
+                    )
+                )
+
+        if filters.search:
+            search_term = f"%{filters.search}%"
+            query = query.where(
+                or_(
+                    model.PersonalTask.title.ilike(search_term),
+                    model.PersonalTask.description.ilike(search_term),
+                )
+            )
+
+        return query
+
+    def _apply_sorting(self, stmt: Select, sorting: common_dto.SortingDto):
+        if sorting.sort_by == "priority":
+            sort_by = self._get_priority_order_case()
+        else:
+            sort_by = getattr(model.PersonalTask, sorting.sort_by)
+        if sorting.order == "asc":
+            stmt = stmt.order_by(asc(sort_by))
+        else:
+            stmt = stmt.order_by(desc(sort_by))
+
+        return stmt
+
+    def _get_priority_order_case(self):
+        """
+            Returns sql case for sorting by priority
+            - CRITICAL = 4
+            - HIGH = 3
+            - MEDIUM = 2
+            - LOW = 1
+        """
+        return case(
+            (model.PersonalTask.priority == TaskPriority.CRITICAL, 4),
+            (model.PersonalTask.priority == TaskPriority.HIGH, 3),
+            (model.PersonalTask.priority == TaskPriority.MEDIUM, 2),
+            (model.PersonalTask.priority == TaskPriority.LOW, 1),
+            else_=0
+        )
